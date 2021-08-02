31fdd2c738053652a7089218af9cb88a8a50bd5a
Abhishek <yadavabhishek241296@gmail.com>
2021-08-02 17:22:39 +0530

    initial commit

---
+++ src/Colors.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const AppTheme = {
+  light: {
+    textColor: "#000",
+    backgroundColor: "#FFF",
+    border: "2px solid 000",
+  },
+  dark: {
+    textColor: "#FFF",
+    backgroundColor: "#333",
+    border: "2px dashed 000",
+  },
+};
+
+export default AppTheme;
---
+++ src/Components/HeroSection.js
@@ -0,0 +1,37 @@
+import React, {useContext} from 'react'
+import ThemeContext from '../Context/ThemeContext'
+import AppTheme from '../Colors'
+
+
+const HeroSection = () => {
+    const theme = useContext(ThemeContext)[0]
+    const currentTheme = AppTheme[theme]
+
+    return(
+        <div
+        style = {{
+            padding: "1rem",
+            backgroundColor:`${currentTheme.backgroundColor}`,
+            color: `${currentTheme.textcolor}`,
+            textAlign: "center"
+        }}
+        >
+        <h1>Context API theme toggler</h1>
+        <p> This is a nice paragraph</p>
+        <button
+        style = {{
+            backgroundColor: "green",
+            padding:"10px 150px",
+            fontSize: "20px",
+            color:"#FFF",
+            border: `${currentTheme.border}`
+        }}
+        >click me</button>
+
+        </div>
+    )
+
+}
+
+export default HeroSection;
+
